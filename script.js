300db494298c821d583aac0acc6c1e6371a7ce63
Keshav-Thathai <thataikeshav873@gmail.com>
2026-02-25 09:57:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let score = 0;
+let timeLeft = 30;
+
+let box = document.createElement("div");
+box.className = "box";
+document.getElementById("gameArea").appendChild(box);
+
+function moveBox() {
+  let x = Math.random() * 300;
+  let y = Math.random() * 300;
+  box.style.left = x + "px";
+  box.style.top = y + "px";
+}
+
+box.onclick = function () {
+  score++;
+  document.getElementById("score").innerText = score;
+
+  document.getElementById("hitSound").currentTime = 0;
+  document.getElementById("hitSound").play();
+
+  moveBox();
+};
+
+function startGame() {
+  moveBox();
+
+  let timer = setInterval(function () {
+    timeLeft--;
+    document.getElementById("time").innerText = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      alert("Game Over! Score: " + score);
+    }
+  }, 1000);
+}
+
